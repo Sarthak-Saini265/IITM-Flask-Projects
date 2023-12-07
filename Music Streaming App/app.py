8f7754b1fb391365469513a8b2fe73aef5d3cb86
Sarthak Saini <saini.sarthak0505@gmail.com>
2023-12-07 11:03:49 +0530

    Update 12/7/2023

--- Music Streaming App/app.py
+++ Music Streaming App/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, redirect
 from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+from pydub import AudioSegment
+from pydub.utils import mediainfo
+import os
 
 app = Flask(__name__)
 api = Api(app)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER_TXT = 'static'
+app.config['UPLOAD_FOLDER_TXT'] = UPLOAD_FOLDER_TXT
 app.app_context().push()
-
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///music_streaming_app.sqlite3"
@@ -46,20 +52,31 @@ class albums(db.Model):
 
 # db.create_all()
 
-with open('static/python_questions.txt', 'r') as file:
-    lyrics_content = file.read()
 # db.session.query(songs).delete()
 # db.session.commit()
 # add_song = songs(name='Star Sky', lyrics=lyrics_content, duration='05:35', mp3='/uploads/Two Steps From Hell - Star Sky.mp3')
 # db.session.add(add_song)
 # db.session.commit()
+# dele = songs.query.filter_by(song_id=3).first()
+# db.session.delete(dele)
+# db.session.commit()
+
+resource_fields_2 = {
+    'username' : fields.String,
+    'password' : fields.String,
+}
+
+acc_post_args = reqparse.RequestParser()
+acc_post_args.add_argument("username", type=str, help = "Username is required", required = True)
+acc_post_args.add_argument("password", type=str, help = "Password is required", required = True)
+
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
 @app.route('/signin', methods=['POST'])
-def products():
+def signin():
     username = request.form['username']
     password = request.form['password']
     # add = login(username=username, password=password, acc_type='General')
@@ -73,6 +90,35 @@ def products():
             return render_template('index.html', trigger_js=True)
     else:
         return render_template('index.html', trigger_js_2=True)
+    
+@app.route('/create_account')
+def create_account():
+    return render_template('user_sign_up.html')
+
+class new_acc(Resource):
+    @marshal_with(resource_fields_2)
+    def post(self):
+        args = acc_post_args.parse_args()
+        new = login(username = args["username"], password = args["password"], acc_type = 'General')
+        db.session.add(new)
+        db.session.commit()
+        return new
+    
+
+
+# @app.route('/signup', methods=['POST'])
+# def signup():
+#     username = request.form['username']
+#     password = request.form['password']
+#     try:
+#         add = login(username=username, password=password, acc_type='General')
+#         db.session.add(add)
+#         db.session.commit()
+#         return redirect(f'/user/{username}')
+#     except:
+#         return render_template('user_sign_up.html', trigger_js=True)
+
+
     
 @app.route('/creator')
 def creator():
@@ -99,28 +145,40 @@ def creator_page(username):
     return render_template('creator_page.html', username=username)
 
 resource_fields = {
-    'id' : fields.Integer,
-    'name' : fields.String,
-    'lyrics' : fields.String,
+    'name' : fields.String
 }
 
 task_post_args = reqparse.RequestParser()
 task_post_args.add_argument("name", type=str, help = "Name is required", required = True)
-task_post_args.add_argument("lyrics", type=str, help = "lyrics are required", required = True)
 
 class Todo(Resource):
     @marshal_with(resource_fields)
-    def post(self, song_id):
-        args = task_post_args.parse_args()
-        task = songs.query.filter_by(song_id=song_id).first()
-        if task:
-            abort(409, message = "This song Id is taken")
-        song = songs(song_id=song_id, name = args["name"], lyrics = args["lyrics"], duration = '03:34', mp3 = '/uploads/snsms.mp3')
+    def post(self):
+        lyr = request.files['txt']
+        os.makedirs(app.config['UPLOAD_FOLDER_TXT'], exist_ok=True)
+        lyr_path = os.path.join(app.config['UPLOAD_FOLDER_TXT'], lyr.filename)
+        print(lyr_path)
+        lyr.save(lyr_path)
+        with open(f'static/{lyr.filename}', 'r') as file_r:
+            lyrics_content = file_r.read()
+        file = request.files['file']
+        os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        print(file_path)
+        file.save(file_path)
+        audio_info = mediainfo(file_path)
+        duration_in_seconds = float(audio_info['duration'])
+        # print('wdwdwewdcwdwfegg')
+        name = request.form['name']
+        # print('njndjkasnsknasdkwmswk')
+        song = songs(name = name, lyrics = lyrics_content, duration = duration_in_seconds, mp3=f'/uploads/{file.filename}')
         db.session.add(song)
         db.session.commit()
-        return song
+        return song, f'Path-{file_path}'
     
-
+@app.route('/upload')
+def upload():
+    return render_template('upload_song.html')
 
 
 @app.route('/user/<username>')
@@ -133,8 +191,8 @@ def user_page(username):
         return 'User not Found'
 
 
-api.add_resource(Todo, "/song/<int:song_id>")
-
+api.add_resource(Todo, "/song/upload")
+api.add_resource(new_acc, "/signup")
 
 
 if __name__ == "__main__":
