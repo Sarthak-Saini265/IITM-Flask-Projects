1181ff41752092afc961d963fb234308f70ecfb7
Sarthak Saini <saini.sarthak0505@gmail.com>
2024-03-09 17:52:07 +0530

    UI Changes Done

--- Music Streaming App/static/lyrics.js
+++ Music Streaming App/static/lyrics.js
@@ -1,13 +1,16 @@
 let lyr = document.getElementById('lyrics');
 let lyr_div = document.getElementById('lyrics_div')
+let overlay = document.getElementById('overlay')
 // console.log(lyr)
-console.log(lyr_div.style.display)
+// console.log(lyr_div.style.display)
 
 function lyrics() {
     if (lyr_div.style.display === 'none' || lyr_div.style.display === '') {
         lyr_div.style.display = 'block';
+        overlay.style.display = 'block';
     } else {
         lyr_div.style.display = 'none';
+        overlay.style.display = 'none';
     }
 }
 
